23e6c6adc244e5a9e60b0b113e141baaa9e77d34
AvaXiaohongDeng <xiaohongdeng196@gmail.com>
2021-02-18 19:05:43 -0500

    almost done
    almost done

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -6,13 +6,13 @@ public class MoveCamera : MonoBehaviour
 {
     public float inputX, inputZ;
     public GameObject cube;
-    public Vector3 cameraOffSet;
+    public Vector3 cameraOffSet;   
 
     // Start is called before the first frame update
     void Start()
     {
         cube = GameObject.Find("Cube");
-        cameraOffSet = new Vector3(-10, 5, -30);
+        cameraOffSet = new Vector3(-10, 5, -30);        
     }
 
 
@@ -20,25 +20,37 @@ public class MoveCamera : MonoBehaviour
     void Update()
     {
         //keep the camera following the cube always
-        transform.position = cube.transform.position + cameraOffSet;
+        CameraFollowCube();
 
-        //control camera by the arrow keys
+        //control the movement of camera by using arrow keys
+        MoveCameraByArrow();
+         
+    }
+
+    //keep the camera following the cube always
+    public void CameraFollowCube()
+    {
+        transform.position = cube.transform.position + cameraOffSet;
+    }
+
+    //control the movement of camera by using arrow keys
+    public void MoveCameraByArrow()
+    {
         inputX = Input.GetAxis("Horizontal");
         inputZ = Input.GetAxis("Vertical");
 
         if (inputX != 0)
             rotate();
         if (inputZ != 0)
-            move();        
+            move();
     }
-
     private void rotate()
     {
-        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime*5f, 0f));
+        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime * 5f, 0f));
     }
 
     private void move()
     {
-        transform.position += transform.forward * inputZ * Time.deltaTime*5f;
+        transform.position += transform.forward * inputZ * Time.deltaTime * 5f;
     }
 }
